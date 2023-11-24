f4ab7477960bb01d88e91e44efde25d6304ce917
Wade Penistone <wade.penistone@ultimate-uk.com>
2023-11-24 20:32:20 +0000

    XR testing page boilerplate

---
+++ playground/src/pages/basics/XR.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import { TresCanvas } from '@tresjs/core'
+</script>
+
+<template>
+  <TresCanvas>
+    <TresMesh>
+      <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+      <TresMeshBasicMaterial color="orange" />
+    </TresMesh>
+  </TresCanvas>
+</template>
--- playground/src/router/routes/basics.ts
+++ playground/src/router/routes/basics.ts
@@ -4,4 +4,9 @@ export const basicsRoutes = [
     name: 'Rigid Body',
     component: () => import('../../pages/basics/RigidBodyDemo.vue'),
   },
+  {
+    path: '/basics/xr',
+    name: 'XR',
+    component: () => import('../../pages/basics/XR.vue'),
+  },
 ]
