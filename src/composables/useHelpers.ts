a2462e3577c978305f9e6e04aaeda047d520b79b
Wade Penistone <333921+Truemedia@users.noreply.github.com>
2024-01-27 15:58:27 +0000

    Helpers composable

---
+++ src/composables/useHelpers.ts
@@ -0,0 +1,8 @@
+export function useHelpers() {
+    /**
+     * Filters to unique entries of an array.
+     */
+    const uniq = <T>(arr: T[]): T[] => Array.from(new Set<T>(arr))
+
+    return {uniq}
+}
