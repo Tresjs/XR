e7aa0ff50acb793721efb0390ca4c78d8f007225
Wade Penistone <333921+Truemedia@users.noreply.github.com>
2024-01-27 22:19:39 +0000

    Watch effects on XR manager

--- src/components/XRManager.vue
+++ src/components/XRManager.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-    import { XRProps } from '../types/xr'
+    import { inject, watchEffect } from 'vue'
+    import { XRProps, XRManagerEvent } from '../types/xr'
     import InteractionManager from './InteractionManager.vue'
-    import { BoxGeometry, Mesh, MeshBasicMaterial } from 'three'
     import { useTresContext } from '@tresjs/core'
     import { useGlobalSessionStore } from '../stores/globalSession'
-    import { XRManagerEvent } from '../types'
-    import { onMounted } from 'vue'
+    import { XRController } from '../classes/XRController'
     const { renderer, camera } = useTresContext()
     // TODO: Grab renderer from tres context and use as GL var
     const globalSessionStore = useGlobalSessionStore()
@@ -19,23 +18,43 @@
         referenceSpace: 'local-floor'
     })
 
-    // TODO: Replace primitives with intended components
-    // Create a box geometry and a basic material
-    const geometry = new BoxGeometry(1, 1, 1)
+    const state = inject('state')
+    const {player, session, controllers} = state
 
-    // Create a mesh with the geometry and material
-    const player = new Mesh(geometry, new MeshBasicMaterial({ color: 0x00ff00 }))
+    watchEffect(() => {
+        const handlers = [0, 1].map((id) => {
+            const target = new XRController(id, renderer.value)
+            const onConnected = () => {
+                state.controllers = [...state.controllers, target]
+            }
+            const onDisconnected = () => {
+                state.controllers = state.controllers.filter((it) => it !== target)
+            }
 
-    // globalSessionStore.$subscribe((mutation, state) => {
-    //     renderer.value.xr = state.session
-    // })
-    onMounted(() => {
+            target.addEventListener('connected', onConnected)
+            target.addEventListener('disconnected', onDisconnected)
+
+            return () => {
+                target.removeEventListener('connected', onConnected)
+                target.removeEventListener('disconnected', onDisconnected)
+            }
+        })
+        // return () => handlers.forEach((cleanup) => cleanup())
+    })
+    //   }, [gl, set])
+    watchEffect(() => {
+        globalSessionStore.$subscribe(() => {
+            state.session = globalSessionStore.session
+        })
+    })
+
+    watchEffect(() => {
         const {foveation} = props
 
         renderer.value.xr.setFoveation(foveation)
     })
 
-    onMounted(() => {
+    watchEffect(() => {
         const {frameRate} = props
 
         try {
@@ -45,60 +64,59 @@
         }
     })
 
-    onMounted(() => {
+    watchEffect(() => {
         const {referenceSpace} = props
 
         renderer.value.xr.setReferenceSpaceType(referenceSpace)
         globalSessionStore.referenceSpaceType = referenceSpace
     })
 
-    onMounted(() => {
-    // if (!session) return void gl.xr.setSession(null!)
+    watchEffect(() => {
+        if (!session) return void renderer.value.xr.setSession(null!)
 
     const handleSessionStart = (nativeEvent: XRManagerEvent) => {
-    //   set(() => ({ isPresenting: true }))
+        state.isPresenting = true
     //   onSessionStartRef.current?.({ nativeEvent: { ...nativeEvent, target: session }, target: session })
     }
     const handleSessionEnd = (nativeEvent: XRManagerEvent) => {
-    //   set(() => ({ isPresenting: false, session: null }))
-    //   globalSessionStore.setState(() => ({ session: null }))
+        Object.assign(state, {isPresenting: false, session: null})
+        globalSessionStore.session = null
     //   onSessionEndRef.current?.({ nativeEvent: { ...nativeEvent, target: session }, target: session })
     }
     const handleVisibilityChange = (nativeEvent: XRSessionEvent) => {
     //   onVisibilityChangeRef.current?.({ nativeEvent, target: session })
     }
     const handleInputSourcesChange = (nativeEvent: XRInputSourceChangeEvent) => {
-    //   const isHandTracking = Object.values(session.inputSources).some((source) => source.hand)
-    //   set(() => ({ isHandTracking }))
+      const isHandTracking = Object.values(session.inputSources).some((source) => source.hand)
+      Object.assign(state, {isHandTracking})
     //   onInputSourcesChangeRef.current?.({ nativeEvent, target: session })
     }
 
     renderer.value.xr.addEventListener('sessionstart', handleSessionStart)
     renderer.value.xr.addEventListener('sessionend', handleSessionEnd)
-    // session.addEventListener('visibilitychange', handleVisibilityChange)
-    // session.addEventListener('inputsourceschange', handleInputSourcesChange)
+    session.addEventListener('visibilitychange', handleVisibilityChange)
+    session.addEventListener('inputsourceschange', handleInputSourcesChange)
 
-    // renderer.value.xr.setSession(session).then(() => {
+    renderer.value.xr.setSession(session).then(() => {
       // on setSession, three#WebXRManager resets foveation to 1
       // so foveation set needs to happen after it
-//       renderer.value.xr.setFoveation(get().foveation)
+      renderer.value.xr.setFoveation(state.foveation)
     })
 
-//     return () => {
-//       gl.xr.removeEventListener('sessionstart', handleSessionStart)
-//       gl.xr.removeEventListener('sessionend', handleSessionEnd)
-//       session.removeEventListener('visibilitychange', handleVisibilityChange)
-//       session.removeEventListener('inputsourceschange', handleInputSourcesChange)
-//     }
+    return () => {
+        renderer.value.xr.removeEventListener('sessionstart', handleSessionStart)
+        renderer.value.xr.removeEventListener('sessionend', handleSessionEnd)
+        session.removeEventListener('visibilitychange', handleVisibilityChange)
+        session.removeEventListener('inputsourceschange', handleInputSourcesChange)
+    }
+})
 //   }, [session, gl.xr, set, get])
-    // })
 </script>
 
 <template>
     <InteractionManager>
         <primitive :object="player" />
-        <Suspense>
-            <!-- <primitive :object="camera" /> -->
-        </Suspense>
+        <primitive v-if="camera" :object="camera" />
+        <primitive v-for="(controller, index) in controllers" :key="index" :object="controller" />
     </InteractionManager>
 </template>
