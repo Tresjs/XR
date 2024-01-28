676ae2b38fdbe9ebc3572a33e6e14670c8d33ed9
Wade Penistone <333921+Truemedia@users.noreply.github.com>
2024-01-28 13:47:56 +0000

    Interaction manager boilerplate

---
+++ src/components/InteractionManager.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+    import { inject } from 'vue'
+
+    const state = inject('state')
+    const {
+        controllers, interactions, hoverState, hasInteraction, getInteraction
+    } = state
+
+//    const events = useThree((state) => state.events)
+//   const get = useThree((state) => state.get)
+//   const raycaster = useThree((state) => state.raycaster)clear
+</script>
+
+<template>
+    <TresGroup>
+        <slot />
+    </TresGroup>
+</template>
