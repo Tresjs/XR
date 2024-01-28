3c7bebf00465138e03b687fd4b567d490b136ea1
Wade Penistone <333921+Truemedia@users.noreply.github.com>
2024-01-28 13:47:28 +0000

    callback demo debugging

--- playground/src/pages/basics/XR.vue
+++ playground/src/pages/basics/XR.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
   import { TresCanvas } from '@tresjs/core'
   import { VRButton, XR } from '@tresjs/xr'
+
+  function onSessionStart() {
+    console.log('session started')
+  }
+  function onError(error) {
+    console.log('error callback', error)
+  }
 </script>
 
 <template>
   <div>
     <VRButton />
     <TresCanvas window-size>
-      <XR>
+      <XR v-bind="{onSessionStart, onError}">
         <TresMesh>
           <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
           <TresMeshBasicMaterial color="blue" />
