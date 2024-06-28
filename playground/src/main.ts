474eda040978a3db4e7992bfd42c1ac9d2db43a4
alvarosabu <alvaro.saburido@gmail.com>
2024-06-28 18:16:07 +0200

    refactor: remove pinia

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,14 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
-import { createPinia } from 'pinia'
 import App from './App.vue'
 import { router } from './router'
 import 'uno.css'
 
 const app = createApp(App)
-const pinia = createPinia()
 
 app.use(router)
-app.use(pinia)
 
 app.mount('#app')
--- playground/src/pages/basics/XR.vue
+++ playground/src/pages/basics/XR.vue
@@ -15,14 +15,14 @@ function onError(error) {
   <div>
     <VRButton />
     <TresCanvas window-size alpha>
-      <TresPerspectiveCamera :position="[0, 0, 5]" />
+      <TresPerspectiveCamera :position="[0, 0, 0]" />
       <XR v-bind="{ onSessionStart, onError }">
-        <TresMesh :position="[-2, 0, 0]">
+        <TresMesh :position="[-2, 0, -5]">
           <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
           <TresMeshBasicMaterial color="blue" />
         </TresMesh>
       </XR>
-      <TresMesh :position="[2, 0, 0]">
+      <TresMesh :position="[2, 0, -5]">
         <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
         <TresMeshBasicMaterial color="orange" />
       </TresMesh>
